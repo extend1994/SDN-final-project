23a2db5cf53a4e6155d3b4112cd7726a33adcefe
TommyHu <tomyhu1995@gmail.com>
2018-06-04 14:01:41 +0800

    Change network connecton policy

--- AP_program/Controller_agent/controller_agent.c
+++ AP_program/Controller_agent/controller_agent.c
@@ -32,65 +32,69 @@ void *controller_agent(void){
 
 	while(1){
 		memset(message, 0, sizeof(25));
-		recv(sockfd, message, MAX_PAYLOAD, 0);
-		log_info("RECV message!!!\n");
-		Header_t output_header;
-		parser_header(message, &output_header);
-		log_debug("ID: %d\n", (int)output_header.ID);
-		log_debug("Type: %d\n", (int)output_header.Type);
-		log_debug("Length: %d\n", (int)output_header.Length);
-		log_debug("Bitmap: 0x%x\n", (int)output_header.Bitmap);
-		int index;
-		for(index = 0; index< output_header.Length; index++){
-			log_debug("No. %d: %d\n", index,output_header.payload[index]);
-		}
-		if(output_header.ID != AP_ID){
-			log_info("Invalid AP ID %d\n", output_header.ID);
-			goto error_AP_ID;
-		}
-		switch(output_header.Type){
-			case HELLO_BACK:{
-				log_info("Hello back message\n");
-				if(parser_hello_back(message) == SUCCESS){
-					log_info("good hello back message\n");
-					conntected = 1; //connected.
+		if(recv(sockfd, message, MAX_PAYLOAD, 0)){
+			log_info("RECV message!!!\n");
+			Header_t output_header;
+			parser_header(message, &output_header);
+			log_debug("ID: %d\n", (int)output_header.ID);
+			log_debug("Type: %d\n", (int)output_header.Type);
+			log_debug("Length: %d\n", (int)output_header.Length);
+			log_debug("Bitmap: 0x%x\n", (int)output_header.Bitmap);
+			int index;
+			for(index = 0; index< output_header.Length; index++){
+				log_debug("No. %d: %d\n", index,output_header.payload[index]);
+			}
+			if(output_header.ID != AP_ID){
+				log_info("Invalid AP ID %d\n", output_header.ID);
+				goto error_AP_ID;
+			}
+			switch(output_header.Type){
+				case HELLO_BACK:{
+					log_info("Hello back message\n");
+					if(parser_hello_back(message) == SUCCESS){
+						log_info("good hello back message\n");
+						conntected = 1; //connected.
+					}
+					break;
 				}
-				break;
-			}
-			case INIT_SET:{
-				log_info("Initial setup message\n");
-				INIT_SET_parameter_t parameter;
-				parser_initial_set(&output_header, &parameter);
-				firmware_set(output_header, &parameter);
-				break;
-			}
-			case SET:{
-				log_info("Set up message\n");
-				SET_parameter_t set_parameter;
-				parser_set(&output_header, &set_parameter);
-				firmware_set(output_header, &set_parameter);
-				break;
-			}
-			case KEEP_ALIVE:{
-				log_info("Keep alive message\n");
-				//send keep alive back message to controller immediately
-				uint8_t send_message[PRE_HEADER_SIZE] = {0};
-				encode_keep_alive(send_message);
-				if(send(sockfd, send_message, PRE_HEADER_SIZE, 0) != PRE_HEADER_SIZE){
-					log_error("Fail to send\n");
-					goto error;
+				case INIT_SET:{
+					log_info("Initial setup message\n");
+					INIT_SET_parameter_t parameter;
+					parser_initial_set(&output_header, &parameter);
+					firmware_set(output_header, &parameter);
+					break;
 				}
-				break;
-			}
-			default:{
-				log_info("No good type 0x%x\n", output_header.Type);
-			        break;
-			}
-		}	
-		
-		error:
-		error_AP_ID:
-		memset(message, 0, MAX_PAYLOAD * sizeof(uint8_t));
+				case SET:{
+					log_info("Set up message\n");
+					SET_parameter_t set_parameter;
+					parser_set(&output_header, &set_parameter);
+					firmware_set(output_header, &set_parameter);
+					break;
+				}
+				case KEEP_ALIVE:{
+					log_info("Keep alive message\n");
+					//send keep alive back message to controller immediately
+					uint8_t send_message[PRE_HEADER_SIZE] = {0};
+					encode_keep_alive(send_message);
+					if(send(sockfd, send_message, PRE_HEADER_SIZE, 0) != PRE_HEADER_SIZE){
+						log_error("Fail to send\n");
+						goto error;
+					}
+					break;
+				}
+				default:{
+					log_info("No good type 0x%x\n", output_header.Type);
+					break;
+				}
+			}	
+			
+			error:
+			error_AP_ID:
+			memset(message, 0, MAX_PAYLOAD * sizeof(uint8_t));
+		}else{
+			log_info("Loss connection, bye ~~~~~\n");
+			exit(1);
+		}
 	}
 
 	log_info("Exit %s\n", __FUNC__);
@@ -116,10 +120,9 @@ int socket_initial(void){
 	info.sin_addr.s_addr = inet_addr(controller_ip);
 	info.sin_port = htons(controller_port);
 
-	if(connect(sockfd, (struct sockaddr_in *)&info, sizeof(info)) == -1){
+	while(connect(sockfd, (struct sockaddr_in *)&info, sizeof(info)) == -1){
 		log_error("Connect error\n");
-		log_error("Exit with error %s\n", __FUNC__);
-		return -1;
+		sleep(3);
 	}
 
 	log_info("Exit %s\n", __FUNC__);
